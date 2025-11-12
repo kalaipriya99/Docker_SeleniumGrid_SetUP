137e67b5b4dfa088b0f029408bf55b59e1b11229
kalaipriya99 <kalaimohan99@gmail.com>
2025-11-13 01:33:18 +0530

    surefire file reportin jenkin fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,6 @@ set -e
 echo "=============================="
 echo "Starting Selenium Maven build"
 echo "=============================="
-# Remove old target folder inside container only
-if [ -d "/app/target" ]; then
-    echo "Removing old target folder..."
-    rm -rf /app/target || true
-fi
 # Run Maven with reports going to /app/reports folder
 mvn clean test -Dextent.output.dir=/app/reports
 
